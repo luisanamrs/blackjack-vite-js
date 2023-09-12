3c88b9f8932be925ace7372399901aa424ae4714
luisana rojas <luisana.maria20@gmail.com>
2023-09-12 13:23:16 -0400

    first commit

---
+++ src/blackjack/usecases/pedir-carta.js
@@ -0,0 +1,14 @@
+/**
+ * Esta función me permite tomar una carta
+ * @param {array<String>} deck es un arreglo de string
+ * @returns {<String>} retorna la carta del deck
+ */
+
+export const pedirCarta = ( deck ) => {
+
+    if ( !deck || deck.length === 0 ) {
+        throw 'No hay cartas en el deck';
+    }
+    const carta = deck.pop();
+    return carta;
+}
